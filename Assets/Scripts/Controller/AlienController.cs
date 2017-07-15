0c8a119aa6a8eca25e8af4f3f965439b6630e79a
DIEGO-PC\Diego <diegoa2000alt@gmail.com>
2017-07-15 09:49:04 -0500

    Mejorar moviento de la nave y colision proyectil-alien

---
+++ Assets/Scripts/Controller/AlienController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Controller
+{
+    public class AlienController : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "bullet")
+            {
+                DestroyObject(collision.gameObject);
+                DestroyObject(gameObject);
+            }
+        }
+
+    }
+}
